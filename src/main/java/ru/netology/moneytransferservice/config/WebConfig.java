7b08471421ef621ddd43ddf8a0c9e0e54685c362
APKonovalov <APKonovalov@alfabank.ru>
2023-11-18 22:04:21 +0300

    fix #6: update allowed pathes

--- src/main/java/ru/netology/moneytransferservice/config/WebConfig.java
+++ src/main/java/ru/netology/moneytransferservice/config/WebConfig.java
@@ -1,14 +1,23 @@
 package ru.netology.moneytransferservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${transfer.path}")
+    private String transferPath;
+
+    @Value("${confirm_operation.path}")
+    private String confirmOperationPath;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+        registry.addMapping(transferPath).allowedMethods(HttpMethod.POST.name());
+        registry.addMapping(confirmOperationPath).allowedMethods(HttpMethod.POST.name());
     }
 }
